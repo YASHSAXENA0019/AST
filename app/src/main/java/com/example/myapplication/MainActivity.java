4da8c83bb17bf84a834f6a1a5df8cb5c23cea2ad
VEADER1005 <71756466+VEADER1005@users.noreply.github.com>
2021-06-05 16:42:58 +0530

    comment2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView nameText;
     private EditText enterName;
       //yash
+    //sahil jain
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
